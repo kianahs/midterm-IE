f3e77799d72a2563f45f8c40d299b95ce75e73c8
kianahs <hadysadegh@gmail.com>
2021-12-02 21:07:51 +0330

    comments finished

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/* get html elements with querySelector by id or class */
 const submitButton = document.querySelector("#submitButton");
 const nameField = document.querySelector(".nameField");
 const gender = document.querySelector("#gender");
@@ -6,7 +7,9 @@ const saveButton = document.querySelector("#saveButton");
 const saved_gender = document.querySelector("#saved_gender");
 const clearButton = document.querySelector("#clearButton");
 
-async function getNameData(name) {
+/*send a Get request to API and get promise then convert json object to javascript object is there is no error else show warning and return  */
+
+async function getNameInformation(name) {
   console.log("sending request to API");
   console.log(name);
 
@@ -27,6 +30,7 @@ async function getNameData(name) {
   }
 }
 
+/* create a html element (div) that contains error message and disapears after 5 seconds then append it to body of html page */
 function showAlert(message) {
   var warning = document.createElement("div");
   warning.setAttribute(
@@ -40,10 +44,15 @@ function showAlert(message) {
   document.body.appendChild(warning);
 }
 
+/* fill prediction section of form after result of API request received */
 function setPrediction(nameData) {
   gender.innerHTML = nameData.gender;
   probability.innerHTML = nameData.probability;
 }
+
+/* first hide save answer from preivous request then 
+get name from input and send it to getNameInformation function to received javaScript object from result of API request then show prediction and show saved answer
+ if  there is any saved answer */
 
 async function sendRequestToAPI(e) {
   hideSavedAnsweContainer();
@@ -57,13 +66,17 @@ async function sendRequestToAPI(e) {
   e.preventDefault();
 
   let nameData;
-  nameData = await getNameData(name);
+  nameData = await getNameInformation(name);
   console.log(nameData);
   if (nameData == null) return;
 
   setPrediction(nameData);
   showSavedAnswerContainer(name);
 }
+
+/* save name and gender to local storage 
+if user predicts gender , predicted gender saved if not gender of response saved in local storage then 
+the saved answer box appears */
 
 function saveInformation(e) {
   console.log("save button clicked!");
@@ -95,6 +108,7 @@ function saveInformation(e) {
   showSavedAnswerContainer(name);
 }
 
+/*if there is value for name in local storage saved answer box appears */
 function showSavedAnswerContainer(name) {
   var storage = localStorage.getItem(name);
   if (storage) {
@@ -103,16 +117,19 @@ function showSavedAnswerContainer(name) {
   }
 }
 
+/* hiding saved answer box */
 function hideSavedAnsweContainer() {
   document.getElementById("saved_answer_container").style.display = "none";
 }
 
+/* if user clicks clear button saved answer box disapears and local storage value of that name deletes */
 function clearSavedAnswer(e) {
   e.preventDefault();
   document.getElementById("saved_answer_container").style.display = "none";
   localStorage.removeItem(nameField.value);
 }
 
+/* if user press keys that related character does not match accepted patterns, avoids filling the input*/
 const $input = nameField;
 const ALLOWED_CHARS_REGEXP = /^[a-zA-Z ]*$/;
 $input.addEventListener("beforeinput", (e) => {
@@ -120,7 +137,7 @@ $input.addEventListener("beforeinput", (e) => {
     e.preventDefault();
   }
 });
-
+/*set which function called if button pressed by adding event listeners to each button and specify function*/
 submitButton.addEventListener("click", sendRequestToAPI);
 saveButton.addEventListener("click", saveInformation);
 clearButton.addEventListener("click", clearSavedAnswer);
